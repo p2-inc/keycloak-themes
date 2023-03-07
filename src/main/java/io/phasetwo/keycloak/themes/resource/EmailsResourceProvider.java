867438345aadd27d68bb1ebc2957ccd5a6ac830a
Garth <244253+xgp@users.noreply.github.com>
2023-03-07 19:20:56 +0100

    updated resources to work without Resteasy injection. updated http request/response classes to keycloak rather than resteasy.

--- src/main/java/io/phasetwo/keycloak/themes/resource/EmailsResourceProvider.java
+++ src/main/java/io/phasetwo/keycloak/themes/resource/EmailsResourceProvider.java
@@ -2,9 +2,7 @@ package io.phasetwo.keycloak.themes.resource;
 
 import javax.ws.rs.*;
 import lombok.extern.jbosslog.JBossLog;
-import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.RealmModel;
 
 @JBossLog
 public class EmailsResourceProvider extends BaseRealmResourceProvider {
@@ -15,9 +13,7 @@ public class EmailsResourceProvider extends BaseRealmResourceProvider {
 
   @Override
   public Object getRealmResource() {
-    RealmModel realm = session.getContext().getRealm();
-    EmailsResource emails = new EmailsResource(realm, session);
-    ResteasyProviderFactory.getInstance().injectProperties(emails);
+    EmailsResource emails = new EmailsResource(session);
     emails.setup();
     return emails;
   }

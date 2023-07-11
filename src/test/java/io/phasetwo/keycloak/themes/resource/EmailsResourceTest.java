619e705133225c6f3a940b32c2c703c6d2939c86
Garth <244253+xgp@users.noreply.github.com>
2023-07-12 00:02:09 +0200

    updated tests to use testcontainers

--- src/test/java/io/phasetwo/keycloak/themes/resource/EmailsResourceTest.java
+++ src/test/java/io/phasetwo/keycloak/themes/resource/EmailsResourceTest.java
@@ -1,45 +1,78 @@
 package io.phasetwo.keycloak.themes.resource;
 
 import static io.phasetwo.keycloak.Helpers.*;
+import static io.phasetwo.keycloak.themes.theme.AttributeTheme.EMAIL_TEMPLATE_ATTRIBUTE_PREFIX;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
-import static org.junit.Assert.assertNotNull;
-import static io.phasetwo.keycloak.themes.theme.AttributeTheme.EMAIL_TEMPLATE_ATTRIBUTE_PREFIX;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-import io.phasetwo.keycloak.KeycloakSuite;
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import java.io.File;
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 import lombok.extern.jbosslog.JBossLog;
-import org.apache.http.entity.ContentType;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.entity.mime.content.StringBody;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.junit.Ignore;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.broker.provider.util.SimpleHttp;
-import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.KeycloakSessionFactory;
-import org.keycloak.models.RealmModel;
 import org.keycloak.representations.idm.RealmRepresentation;
+import org.testcontainers.junit.jupiter.Container;
 
 @JBossLog
 public class EmailsResourceTest {
 
-  @ClassRule public static KeycloakSuite server = KeycloakSuite.SERVER;
+  static List<File> getDeps() {
+    List<File> dependencies =
+        Maven.resolver()
+            .loadPomFromFile("./pom.xml")
+            .resolve("com.github.spullara.mustache.java:compiler")
+            .withoutTransitivity()
+            .asList(File.class);
+    return dependencies;
+  }
+
+  @Container
+  public static final KeycloakContainer container =
+      new KeycloakContainer("quay.io/phasetwo/keycloak-crdb:22.0.0")
+          .withContextPath("/auth")
+          .withReuse(true)
+          .withProviderClassesFrom("target/classes")
+          .withEnv("KC_SPI_EMAIL_TEMPLATE_PROVIDER", "freemarker-plus-mustache")
+          .withEnv("KC_SPI_EMAIL_TEMPLATE_FREEMARKER_PLUS_MUSTACHE_ENABLED", "true")
+          .withDisabledCaching()
+          .withProviderLibsFrom(getDeps());
+
+  @BeforeAll
+  public static void beforeAll() {
+    container.start();
+  }
+
+  @AfterAll
+  public static void afterAll() {
+    container.stop();
+  }
 
   CloseableHttpClient httpClient = HttpClients.createDefault();
 
+  static Keycloak getKeycloak() {
+    return container.getKeycloakAdminClient();
+  }
+
   String baseUrl(String realm) {
-    return String.format("%s/realms/%s/emails", server.getAuthUrl(), realm);
+    return String.format("%s/realms/%s/emails", container.getAuthServerUrl(), realm);
   }
 
   String url(String realm, String... segments) {
@@ -51,28 +84,28 @@ public class EmailsResourceTest {
   }
 
   @Test
-  @Ignore
   public void testGetTemplates() throws Exception {
-    Keycloak keycloak = server.client();
+    Keycloak keycloak = getKeycloak();
     // GET /templates
-    SimpleHttp.Response response = SimpleHttp.doGet(url("master", "templates"), httpClient)
-                                   .auth(keycloak.tokenManager().getAccessTokenString())
-                                   .asResponse();
+    SimpleHttp.Response response =
+        SimpleHttp.doGet(url("master", "templates"), httpClient)
+            .auth(keycloak.tokenManager().getAccessTokenString())
+            .asResponse();
     assertThat(response.getStatus(), is(200));
-    Map<String,String> templates = response.asJson(new TypeReference<Map<String,String>>() {});
+    Map<String, String> templates = response.asJson(new TypeReference<Map<String, String>>() {});
     for (String key : EmailsResource.EMAIL_TEMPLATES.keySet()) {
-      assertNotNull(templates.get(key));
+      assertThat(templates.get(key), notNullValue());
       assertThat(templates.get(key), is(EmailsResource.EMAIL_TEMPLATES.get(key).toString()));
     }
   }
 
   @Test
-  @Ignore
   public void testGetNonexistentTemplate() throws Exception {
-    Keycloak keycloak = server.client();
-    SimpleHttp.Response response = SimpleHttp.doGet(url("master", "templates", "text", "foo-bar"), httpClient)
-                                   .auth(keycloak.tokenManager().getAccessTokenString())
-                                   .asResponse();
+    Keycloak keycloak = getKeycloak();
+    SimpleHttp.Response response =
+        SimpleHttp.doGet(url("master", "templates", "text", "foo-bar"), httpClient)
+            .auth(keycloak.tokenManager().getAccessTokenString())
+            .asResponse();
     assertThat(response.getStatus(), is(404));
   }
 
@@ -84,7 +117,7 @@ public class EmailsResourceTest {
   @Test
   public void testGetUpdateTemplateNonMaster() throws Exception {
     String realmName = "test";
-    Keycloak keycloak = server.client();
+    Keycloak keycloak = getKeycloak();
     RealmRepresentation r = new RealmRepresentation();
     r.setRealm(realmName);
     r.setEnabled(true);
@@ -93,19 +126,20 @@ public class EmailsResourceTest {
   }
 
   void testGetUpdateTemplate(String realmName) throws Exception {
-    Keycloak keycloak = server.client();
+    Keycloak keycloak = getKeycloak();
     RealmRepresentation r = keycloak.realm(realmName).toRepresentation();
     r.setEmailTheme("attributes");
     keycloak.realm(realmName).update(r);
 
     // GET /templates/text/email-verification
-    SimpleHttp.Response response = SimpleHttp.doGet(url(realmName, "templates", "text", "email-verification"), httpClient)
-                                   .auth(keycloak.tokenManager().getAccessTokenString())
-                                   .asResponse();
+    SimpleHttp.Response response =
+        SimpleHttp.doGet(url(realmName, "templates", "text", "email-verification"), httpClient)
+            .auth(keycloak.tokenManager().getAccessTokenString())
+            .asResponse();
     assertThat(response.getStatus(), is(200));
     String template = response.asString();
     assertThat(template, containsString("Someone has created"));
-    
+
     // PUT /templates/text/email-verification
     String templatePlus = template + "\n\nfoo bar";
     HttpPut put = new HttpPut(url(realmName, "templates", "text", "email-verification"));
@@ -114,27 +148,34 @@ public class EmailsResourceTest {
     builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
     builder.addPart("template", templateBody);
     put.setEntity(builder.build());
-    put.addHeader("Authorization", "Bearer "+keycloak.tokenManager().getAccessTokenString());
+    put.addHeader("Authorization", "Bearer " + keycloak.tokenManager().getAccessTokenString());
     HttpResponse resp = httpClient.execute(put);
     assertThat(resp.getStatusLine().getStatusCode(), is(204));
 
     // GET /templates/text/email-verification
-    response = SimpleHttp.doGet(url(realmName, "templates", "text", "email-verification"), httpClient)
-               .auth(keycloak.tokenManager().getAccessTokenString())
-               .asResponse();
+    response =
+        SimpleHttp.doGet(url(realmName, "templates", "text", "email-verification"), httpClient)
+            .auth(keycloak.tokenManager().getAccessTokenString())
+            .asResponse();
     assertThat(response.getStatus(), is(200));
     template = response.asString();
     assertThat(template, is(templatePlus));
     assertThat(template, containsString("foo bar"));
-    
+
     // update with a realm attribute
     templatePlus = templatePlus + "\n\ndog cat";
-    updateRealmAttribute(server.getKeycloak(), realmName, String.format("%s.%s/%s", EMAIL_TEMPLATE_ATTRIBUTE_PREFIX, "text", "email-verification.mustache"), templatePlus);
+    updateRealmAttribute(
+        getKeycloak(),
+        realmName,
+        String.format(
+            "%s.%s/%s", EMAIL_TEMPLATE_ATTRIBUTE_PREFIX, "text", "email-verification.mustache"),
+        templatePlus);
 
     // GET /templates/text/email-verification
-    response = SimpleHttp.doGet(url(realmName, "templates", "text", "email-verification"), httpClient)
-               .auth(keycloak.tokenManager().getAccessTokenString())
-               .asResponse();
+    response =
+        SimpleHttp.doGet(url(realmName, "templates", "text", "email-verification"), httpClient)
+            .auth(keycloak.tokenManager().getAccessTokenString())
+            .asResponse();
     assertThat(response.getStatus(), is(200));
     template = response.asString();
     assertThat(template, is(templatePlus));

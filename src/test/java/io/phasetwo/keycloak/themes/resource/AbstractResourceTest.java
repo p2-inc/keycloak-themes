d4d4e545984e800bd43db721c903e11ffbabd8c9
Garth <244253+xgp@users.noreply.github.com>
2023-12-01 19:59:08 +0100

    version 23 bump (#16)
    * merged test resources into AbstractResourceTest. upgraded to 23. set image version by system variable.
    
    * added some logging for the kc bug report
    
    * workaround for FormBodyHandler not being set on PUT
    
    * bump pom versions

---
+++ src/test/java/io/phasetwo/keycloak/themes/resource/AbstractResourceTest.java
@@ -0,0 +1,67 @@
+package io.phasetwo.keycloak.themes.resource;
+
+import org.keycloak.admin.client.Keycloak;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.testcontainers.junit.jupiter.Container;
+
+public abstract class AbstractResourceTest {
+
+  public static final String KEYCLOAK_IMAGE = String.format("quay.io/phasetwo/keycloak-crdb:%s", System.getProperty("keycloak-version", "23.0.0"));
+
+  static final String[] deps = {
+    "com.github.spullara.mustache.java:compiler"
+  };
+
+  static List<File> getDeps() {
+    List<File> dependencies = new ArrayList<File>();
+    for (String dep : deps) {
+      dependencies.addAll(getDep(dep));
+    }
+    return dependencies;
+  }
+
+  static List<File> getDep(String pkg) {
+    return Maven.resolver()
+            .loadPomFromFile("./pom.xml")
+            .resolve(pkg)
+            .withoutTransitivity()
+            .asList(File.class);
+  }
+
+  @Container
+  public static final KeycloakContainer container =
+      new KeycloakContainer(KEYCLOAK_IMAGE)
+      .withContextPath("/auth")
+      .withReuse(true)
+      .withProviderClassesFrom("target/classes")
+      .withEnv("KC_SPI_EMAIL_TEMPLATE_PROVIDER", "freemarker-plus-mustache")
+      .withEnv("KC_SPI_EMAIL_TEMPLATE_FREEMARKER_PLUS_MUSTACHE_ENABLED", "true")
+      .withDisabledCaching()
+      .withProviderLibsFrom(getDeps());
+
+
+  @BeforeAll
+  public static void beforeAll() {
+    container.start();
+  }
+
+  @AfterAll
+  public static void afterAll() {
+    container.stop();
+  }
+
+  protected CloseableHttpClient httpClient = HttpClients.createDefault();
+
+  public static Keycloak getKeycloak() {
+    return container.getKeycloakAdminClient();
+  }
+
+}

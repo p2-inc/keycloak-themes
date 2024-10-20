31bb37a9fc1fdd18464a97edf84ff877c2acfd90
Garth <244253+xgp@users.noreply.github.com>
2024-10-20 18:10:13 +0200

    cache key includes date for 1 day expiration

--- src/main/java/io/phasetwo/keycloak/email/OverridableEmailSenderProvider.java
+++ src/main/java/io/phasetwo/keycloak/email/OverridableEmailSenderProvider.java
@@ -1,6 +1,9 @@
 package io.phasetwo.keycloak.email;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.jbosslog.JBossLog;
 import org.infinispan.Cache;
 import org.keycloak.connections.infinispan.InfinispanConnectionProvider;
@@ -16,6 +19,7 @@ public class OverridableEmailSenderProvider extends DefaultEmailSenderProvider {
   private final Map<String, String> conf;
   private final Integer maxEmails;
   private final Cache<String, Integer> counterCache;
+  private final String cacheKey;
 
   public OverridableEmailSenderProvider(
       KeycloakSession session, Map<String, String> conf, Integer maxEmails) {
@@ -25,6 +29,7 @@ public class OverridableEmailSenderProvider extends DefaultEmailSenderProvider {
     this.maxEmails = maxEmails;
     this.counterCache =
         session.getProvider(InfinispanConnectionProvider.class).getCache("counterCache", true);
+    this.cacheKey = getCacheKey();
   }
 
   private boolean useOverride(Map<String, String> config) {
@@ -33,15 +38,16 @@ public class OverridableEmailSenderProvider extends DefaultEmailSenderProvider {
 
   private String getCacheKey() {
     if (session.getContext().getRealm() != null) {
+      SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
       return String.format(
-          "ext-email-override-emailCounter-%s", session.getContext().getRealm().getName());
+          "ext-email-override-emailCounter-%s-%s",
+          session.getContext().getRealm().getName(), formatter.format(new Date()));
     } else {
       return null;
     }
   }
 
   private boolean canSend() {
-    String cacheKey = getCacheKey();
     if (cacheKey == null) return true;
     Integer count = counterCache.get(cacheKey);
     if (count == null || count <= maxEmails) return true;
@@ -49,9 +55,10 @@ public class OverridableEmailSenderProvider extends DefaultEmailSenderProvider {
   }
 
   private Integer increment() {
-    String cacheKey = getCacheKey();
     if (cacheKey == null) return 0;
-    else return counterCache.compute(cacheKey, (key, value) -> (value == null) ? 1 : value + 1);
+    else
+      return counterCache.compute(
+          cacheKey, (key, value) -> (value == null) ? 1 : value + 1, 1, TimeUnit.DAYS);
   }
 
   @Override
@@ -65,9 +72,9 @@ public class OverridableEmailSenderProvider extends DefaultEmailSenderProvider {
       if (canSend()) {
         super.send(conf, user, subject, textBody, htmlBody);
         Integer count = increment();
-        log.infof("Email count %d for %s", count, getCacheKey());
+        log.infof("Email count %d for %s", count, cacheKey);
       } else {
-        log.infof("Unable to send email for limit %d %s", maxEmails, getCacheKey());
+        log.infof("Unable to send email for limit %d %s", maxEmails, cacheKey);
       }
     }
   }
@@ -83,9 +90,9 @@ public class OverridableEmailSenderProvider extends DefaultEmailSenderProvider {
       if (canSend()) {
         super.send(conf, address, subject, textBody, htmlBody);
         Integer count = increment();
-        log.infof("Email count %d for %s", count, getCacheKey());
+        log.infof("Email count %d for %s", count, cacheKey);
       } else {
-        log.infof("Unable to send email for limit %d %s", maxEmails, getCacheKey());
+        log.infof("Unable to send email for limit %d %s", maxEmails, cacheKey);
       }
     }
   }

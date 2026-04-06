74f2b80465fd5f27a51d0cca255cbccb54f8498e
pnzrp2 <pnzr@phasetwo.io>
2026-04-06 12:41:11 -0600

    add email theme

--- themes/attributes-v3/vite.config.ts
+++ themes/attributes-v3/vite.config.ts
@@ -5,46 +5,61 @@ import { keycloakify } from "keycloakify/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-import { fileURLToPath } from 'node:url';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-import { playwright } from '@vitest/browser-playwright';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+import { fileURLToPath } from "node:url";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+import { playwright } from "@vitest/browser-playwright";
+const dirname =
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react(), keycloakify({
-    accountThemeImplementation: "none",
-    themeName: ["attributes-v3"],
-    themeVersion: "0.0.1",
-    keycloakifyBuildDirPath: path.resolve(dirname, "../../target/attributes-v3-keycloakify"),
-    keycloakVersionTargets: {
-      "22-to-25": false,
-      "all-other-versions": "attributes-v3-theme.jar"
-    }
-  }), tailwindcss()],
+  plugins: [
+    react(),
+    keycloakify({
+      accountThemeImplementation: "none",
+      themeName: ["attributes-v3"],
+      themeVersion: "0.0.1",
+      keycloakifyBuildDirPath: path.resolve(
+        dirname,
+        "../../target/attributes-v3-keycloakify",
+      ),
+      keycloakVersionTargets: {
+        "22-to-25": false,
+        "all-other-versions": "attributes-v3-theme.jar",
+      },
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(dirname, "src")
-    }
+      "@": path.resolve(dirname, "src"),
+    },
   },
   test: {
-    projects: [{
-      extends: true,
-      plugins: [
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: playwright({}),
-          instances: [{
-            browser: 'chromium'
-          }]
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          storybookTest({
+            configDir: path.join(dirname, ".storybook"),
+          }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: playwright({}),
+            instances: [
+              {
+                browser: "chromium",
+              },
+            ],
+          },
+          setupFiles: [".storybook/vitest.setup.ts"],
         },
-        setupFiles: ['.storybook/vitest.setup.ts']
-      }
-    }]
-  }
+      },
+    ],
+  },
 });
